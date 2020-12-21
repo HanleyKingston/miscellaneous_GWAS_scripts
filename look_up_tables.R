d6136a27408ffd3568060590d2e043fb4ac70470
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2020-12-20 21:16:39 -0800

    Update look_up_tables.R

--- look_up_tables.R
+++ look_up_tables.R
@@ -13,22 +13,26 @@ AAOxAPOE <- read.table("/acct/hkings/NIAGADS/analyses/AAO/AAO_xE4E2_4PCs/SNP_pea
 
 
 #P_values:
-E2_noPCs_P <- read.table("/acct/hkings/NIAGADS/analyses/APOE/E2_carrier/E2_carrier_noPCs/assoc.rds")
-E2_4PCs_P <- read.table("/acct/hkings/NIAGADS/analyses/APOE/E2_carrier/E2_carrier_4PCs/assoc.rds")
-E4_noPCs_P <- read.table("/acct/hkings/NIAGADS/analyses/APOE/E4_carrier/E4_carrier_noPCs/assoc.rds")
-E4_4PCs_P <- read.table("/acct/hkings/NIAGADS/analyses/APOE/E4_carrier/E4_carrier_4PCs/assoc.rds")
-AD_P <- read.table("/acct/hkings/NIAGADS/analyses/AD/AD_4PCs/assoc.rds")
-ADxAPOE_P <- read.table("/acct/hkings/NIAGADS/analyses/AD/AD_xE4E2_4PCs/assoc.rds")
-AAO_P <- read.table("/acct/hkings/NIAGADS/analyses/AAO/AAO_4PCs/assoc.rds")
-AAOxAPOE_P <- read.table("/acct/hkings/NIAGADS/analyses/AAO/AAO_xE4E2_4PCs/assoc.rds")
+E2_noPCs_P <- readRDS("/acct/hkings/NIAGADS/analyses/APOE/E2_carrier/E2_carrier_noPCs/assoc.rds")
+E2_4PCs_P <- readRDS("/acct/hkings/NIAGADS/analyses/APOE/E2_carrier/E2_carrier_4PCs/assoc.rds")
+E4_noPCs_P <- readRDS("/acct/hkings/NIAGADS/analyses/APOE/E4_carrier/E4_carrier_noPCs/assoc.rds")
+E4_4PCs_P <- readRDS("/acct/hkings/NIAGADS/analyses/APOE/E4_carrier/E4_carrier_4PCs/assoc.rds")
+AD_P <- readRDS("/acct/hkings/NIAGADS/analyses/AD/AD_4PCs/assoc.rds")
+ADxAPOE_P <- readRDS("/acct/hkings/NIAGADS/analyses/AD/AD_xE4E2_4PCs/assoc.rds")
+AAO_P <- readRDS("/acct/hkings/NIAGADS/analyses/AAO/AAO_4PCs/assoc.rds")
+AAOxAPOE_P <- readRDS("/acct/hkings/NIAGADS/analyses/AAO/AAO_xE4E2_4PCs/assoc.rds")
+
 
 get_p <- function(hit_from, p_from, thresh){
+    p.list <- c()
     hit_from_thresh <- hit_from[hit_from$Score.pval < thresh, ]
     for(row in 1:nrow(hit_from_thresh)){
       hit_from_row <- hit_from_thresh[row,]
-      p_from_row <- p_from[row, ]
-      p.val <- p_from_row[p_from_row$chr == hit_from_row$chr ][p_from_row$pos == hit_from_row$pos, "Score.pval", drop = TRUE]
-      print(p.val)
+      p.val <- p_from[p_from$chr == hit_from_row$chr & p_from$pos == hit_from_row$pos, "Score.pval", drop = TRUE]
+      p.list <- append(p.list, p.val)
+    }
+    return(p.list)
+}
       
-      
-      
+get_p(E2_noPCs, E4_noPCs_P, 5e-6)
+
