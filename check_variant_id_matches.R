6ebb3c2d98643e61e336572ce9e35678847ec8d5
HanleyKingston <64234987+HanleyKingston@users.noreply.github.com>
2021-01-04 13:42:13 -0800

    Update and rename check_variant_id_matches.R to check_variant_id_matches

--- check_variant_id_matches.R
+++
@@ -1,2 +0,0 @@
-rand <- SNPs[sample(1:nrow(SNPs),1), "variant.id"]
-SNPs[SNPs$variant.id == rand, "pos"] == variants_gds[variants_gds$variant.id == rand, "pos"] & SNPs[SNPs$variant.id == rand, "chrom"] == variants_gds[variants_gds$variant.id == rand, "chr"] 
